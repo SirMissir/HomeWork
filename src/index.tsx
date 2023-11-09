47afbbaad8e58c65f74863b179a34fea51a9b3f3
SirMissir <pkv_96@icloud.com>
2023-11-09 23:26:10 +0300

    feat: add HW3

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import "./s1-main/App.module.css";
+import App from "./s1-main/App";
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
