c82893ed288a1d06c8c6a1fda195430ec92b4f33
SirMissir <pkv_96@icloud.com>
2024-03-22 18:00:03 +0300

    refactor: reducer in HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,31 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload == 'up'){
+                state.sort((a, b) => a.name.localeCompare(b.name))
+            }else
+            {
+                state.sort((a, b) => b.name.localeCompare(a.name))
+            }
+            return state// need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el=>el.age >= action.payload) // need to fix
         }
         default:
             return state
     }
 }
+
+// export type todolistReducerType=removeTodoListACType
+// export type removeTodoListACType=ReturnType<typeof removeTodolistAC>
+// export const removeTodolistAC =(id:string)=>{
+//     return{
+//         type: 'REMOVE-TODOLIST',
+//         payload:{id}
+//     }as const
+// }
